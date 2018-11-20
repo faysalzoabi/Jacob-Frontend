68741bf0bcb81c72dd822e5e1049c61a8168d283
Daniele Roncaglioni <roncaglionidaniele>
2018-11-20 14:10:07 +0100

    Set up frontend

--- app/src/App.js
+++ app/src/App.js
@@ -1,30 +1,25 @@
-import React, { Component } from 'react';
-import { Route, Switch } from "react-router-dom";
+import React, {Component} from 'react';
+import {Route, Switch} from "react-router-dom";
 import Header from "./components/header"
+
 class App extends Component {
-  render() {
+  render () {
     console.log("token in app render", localStorage.getItem('token'))
     if (localStorage.getItem('token')) {
       return (
         <div className="App">
-          <Switch>
-            <Header />
-            <h1>PLACEHOLDER FOR COMPONENTS WITH TOKEN</h1>
-
-          </Switch>
+          <Header/>
         </div>
       )
     }
     else {
       return (
-        < div className="App" >
-          <Switch>
-            <Header />
+        < div className="App">
+          <Header/>
 
-            <h1>PLACEHOLDER FOR COMPONENTS no TOKEN</h1>
+          <h1>PLACEHOLDER FOR COMPONENTS no TOKEN</h1>
 
-          </Switch>
-        </div >
+        </div>
       )
     }
   };
