c2572c108dc63b891ee167c2188543d99996cfc9
Daniele Roncaglioni <roncaglionidaniele>
2018-11-23 11:42:54 +0100

    Login Logout

--- app/src/containers/Login/index.js
+++ app/src/containers/Login/index.js
@@ -4,7 +4,6 @@ import {withRouter} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import {withStyles} from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
 import {loginUser} from '../../store/actions/authActions';
 import Error from '../Error/';
 import {AwesomeButton} from 'react-awesome-button';
--- app/src/containers/Results/index.js
+++ app/src/containers/Results/index.js
@@ -2,10 +2,27 @@ import React, {Component} from 'react';
 import './index.css';
 
 class Results extends Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      textareaVal: ''
+    }
+  }
+
+
+  handler = (event) => {
+    console.log("dfdfd",window.getSelection().toString())
+    this.setState({textareaVal: window.getSelection().toString()})
+  }
+
   render () {
+    console.log("text ", this.state.textareaVal)
     return (
       <div className="main-results">
-        Results
+        <div onMouseUp={(e) => this.handler(e)}>
+          Hello World
+        </div>
       </div>)
   }
 }
