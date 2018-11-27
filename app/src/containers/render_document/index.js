21f4f0d31538dff507783a992bf249ff084391cc
Ian Stump <ianstump@gmail.com>
2018-11-27 15:16:45 +0100

    created render document

---
+++ app/src/containers/render_document/index.js
@@ -0,0 +1,135 @@
+import React, { Component } from 'react';
+import "./index.css"
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import SaveIcon from '@material-ui/icons/Save';
+import FormControl from "@material-ui/core/FormControl/FormControl";
+import InputLabel from "@material-ui/core/InputLabel/InputLabel";
+import Select from "@material-ui/core/Select/Select";
+import MenuItem from "@material-ui/core/MenuItem/MenuItem";
+import { connect } from 'react-redux';
+import { fetchTagsAndDocRefs } from "../../store/actions/tagsActions"
+
+
+const styles = theme => ({
+    button: {
+        margin: theme.spacing.unit,
+    },
+    leftIcon: {
+        marginRight: theme.spacing.unit,
+    },
+    rightIcon: {
+        marginLeft: theme.spacing.unit,
+    },
+    iconSmall: {
+        fontSize: 20,
+    },
+    formControl: {
+        margin: theme.spacing.unit,
+        minWidth: 200,
+
+    },
+});
+
+class Render_document extends Component {
+
+    state = {
+        highlighted_text: "",
+        tag: {},
+        documentID: "",
+    }
+
+    getHTMLOfSelection = () => {
+        let range;
+        if (document.selection && document.selection.createRange) {
+            range = document.selection.createRange();
+            return range.htmlText;
+        }
+        else if (window.getSelection) {
+            let selection = window.getSelection();
+            if (selection.rangeCount > 0) {
+                range = selection.getRangeAt(0);
+                let clonedSelection = range.cloneContents();
+                let div = document.createElement('div');
+                div.appendChild(clonedSelection);
+                return div.innerHTML;
+            }
+            else {
+                return '';
+            }
+        }
+        else {
+            return '';
+        }
+    }
+
+    highlightSelected = () => {
+        let html = this.getHTMLOfSelection()
+        console.log(html)
+        this.setState({
+            highlighted_text: html,
+        })
+    }
+    handler = () => {
+        this.highlightSelected()
+    }
+    allDocumentHandler = () => {
+        //  console.log(document.getElementById("text").focus())
+    }
+    render() {
+        const { classes } = this.props;
+        return (
+            <div className="container">
+                <div className="leftPanel"
+                >
+                    <p onMouseUp={this.handler}>
+                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                            et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                            aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+                            dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                            deserunt mollit anim id est laborum."
+                    </p>
+                </div>
+                <div className="rightPanel">
+                    <Button variant="outlined" color="secondary" className={classes.button} onClick={this.allDocumentHandler}
+                    >
+                        Select all text of the Document
+                    </Button>
+                    <h1>Selected text:</h1>
+                    <p>{this.state.highlighted_text}</p>
+                    <form className={classes.root} autoComplete="off">
+                        <FormControl className={classes.formControl}>
+                            <InputLabel htmlFor="age-simple">Tags</InputLabel>
+                            <Select
+                                value={this.state.tag}
+                                onChange={this.handleChange}
+                                inputProps={{
+                                    name: 'tag',
+                                }}
+                            >
+                                {
+                                    this.props.tags.map((tag, index) => {
+                                        return <MenuItem key={index} value={tag.name}>{tag.name}</MenuItem>
+                                    })
+                                }
+                            </Select>
+                        </FormControl>
+                    </form>
+                    <Button variant="contained" size="small" className={classes.button}>
+                        <SaveIcon />
+                        Save
+                </Button>
+                </div>
+            </div>
+        );
+    } componentDidMount = () => {
+        this.props.dispatch(fetchTagsAndDocRefs())
+    }
+}
+const mapStateToProps = state => {
+    return {
+        tags: state.tags,
+    };
+};
+
+export default connect(mapStateToProps)(withStyles(styles)(Render_document));
