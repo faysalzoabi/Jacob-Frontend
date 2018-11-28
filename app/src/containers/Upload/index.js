72bc7e70e9a6143eefb9f1e55983162229416059
Ian Stump <ianstump@gmail.com>
2018-11-28 10:49:02 +0100

    fixed text

--- app/src/containers/Upload/index.js
+++ app/src/containers/Upload/index.js
@@ -1,12 +1,12 @@
-import React, {Component} from 'react';
-import {FilePond, File, registerPlugin} from 'react-filepond';
+import React, { Component } from 'react';
+import { FilePond, File, registerPlugin } from 'react-filepond';
 import 'filepond/dist/filepond.min.css';
 import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation';
 import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.min.css';
 import "./index.css"
-import {baseAPIUrl} from './../../store/constants';
+import { baseAPIUrl } from './../../store/constants';
 
 registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
 
@@ -16,11 +16,11 @@ class Upload extends Component {
     files: []
   }
 
-  handleInit () {
+  handleInit() {
     console.log('FilePond instance has initialised', this.pond);
   }
 
-  render () {
+  render() {
     const token = localStorage.getItem("token").slice(1, -1)
     console.log(token)
     return (
@@ -28,35 +28,31 @@ class Upload extends Component {
 
         {/* Pass FilePond properties as attributes */}
         <FilePond ref={ref => this.pond = ref}
-                  allowMultiple={false}
-                  server=
-                    {
-
-                      {
-                        url: `${baseAPIUrl}file/upload/`,
-                        process: {
-                          headers: {
-                            Authorization: `Bearer ${token}`
-
-                          },
-
-                        },
-                      }
-                    }
-
-                  className="filepond"
-                  oninit={() => this.handleInit()}
-                  onupdatefiles={(fileItems) => {
-                    // Set current file objects to this.state
-                    this.setState({
-                      files: fileItems.map(fileItem => fileItem.file)
-                    });
-                  }}
+          allowMultiple={false}
+          server=
+          {
+            {
+              url: `${baseAPIUrl}file/upload/`,
+              process: {
+                headers: {
+                  Authorization: `Bearer ${token}`
+                },
+              },
+            }
+          }
+          className="filepond"
+          oninit={() => this.handleInit()}
+          onupdatefiles={(fileItems) => {
+            // Set current file objects to this.state
+            this.setState({
+              files: fileItems.map(fileItem => fileItem.file)
+            });
+          }}
         >
 
           {/* Update current files  */}
           {this.state.files.map(file => (
-            <File key={file} src={file} origin="local"/>
+            <File key={file} src={file} origin="local" />
           ))}
 
         </FilePond>
