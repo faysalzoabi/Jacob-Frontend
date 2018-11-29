8b4b3f0407ec24e233c5da7ca88c6949ac070ba0
Daniele Roncaglioni <roncaglionidaniele>
2018-11-29 15:31:26 +0100

    refactored redux state management and frontend

--- app/src/containers/RenderDocument/index.js
+++ app/src/containers/RenderDocument/index.js
@@ -6,79 +6,88 @@ import Typography from '@material-ui/core/Typography';
 
 class RenderDocument extends Component {
 
-  state = {
-    selected_text: "",
-    pdf_documents: "",
-  }
-
-  getHTMLOfSelection = () => {
-    let range;
-    if (document.selection && document.selection.createRange) {
-      range = document.selection.createRange();
-      return range.htmlText;
+    state = {
+        selected_text: "",
+        pdf_documents: "",
     }
-    else if (window.getSelection) {
-      let selection = window.getSelection();
-      if (selection.rangeCount > 0) {
-        range = selection.getRangeAt(0);
-        let clonedSelection = range.cloneContents();
-        let div = document.createElement('div');
-        div.appendChild(clonedSelection);
-        return div.innerHTML;
-      }
-      else {
-        return '';
-      }
+
+    getHTMLOfSelection = () => {
+        let range;
+        if (document.selection && document.selection.createRange) {
+            range = document.selection.createRange();
+            return range.htmlText;
+        }
+        else if (window.getSelection) {
+            let selection = window.getSelection();
+            if (selection.rangeCount > 0) {
+                range = selection.getRangeAt(0);
+                let clonedSelection = range.cloneContents();
+                let div = document.createElement('div');
+                div.appendChild(clonedSelection);
+                return div.innerHTML;
+            }
+            else {
+                return '';
+            }
+        }
+        else {
+            return '';
+        }
     }
-    else {
-      return '';
-    }
-  }
-
-  onSelectText = () => {
-    let html = this.getHTMLOfSelection();
-    this.setState({
-      selected_text: html,
-    })
-  };
 
 
-  render () {
-    if (this.props.isDisplayedinDatapoints) {
-      return (
-        <div className="container">
-          <Paper className="datapoints">
-            <div className="textDoc" onMouseUp={this.onSelectText}>
-              <Typography variant="body1" gutterBottom>
-                {this.props.pdf.text}
-              </Typography>
-            </div>
-          </Paper>
-        </div>
-      );
+    onSelectText = () => {
+        let html = this.getHTMLOfSelection();
+        this.setState({
+            selected_text: html,
+        })
+    };
 
-    } else {
-      return (
-        <div className="container">
-          <Paper className="leftPanel">
-            <div className="textDoc" onMouseUp={this.onSelectText}>
-              <Typography variant="body1" gutterBottom>
-                {this.props.pdf.text}
-              </Typography>
-            </div>
-          </Paper>
 
-          <div className="rightPanel">
-            <Sidebar selected_text={this.state.selected_text} pdf_document={this.state.pdf_document}
-                     id={this.props.pdf.id}/>
-          </div>
-
-        </div>
-      );
+    resetSelection = () => {
+            this.setState({
+            selected_text: ""
+        });
 
     }
 
-  }
+
+    render () {
+        if (this.props.isDisplayedinDatapoints) {
+            return (
+              <div className="container">
+                  <Paper className="datapoints">
+                      <div className="textDoc" onMouseUp={this.onSelectText}>
+                          <Typography variant="body1" gutterBottom>
+                              {this.props.pdf.text}
+                          </Typography>
+                      </div>
+                  </Paper>
+              </div>
+            );
+
+        } else {
+            return (
+              <div className="container">
+                  <Paper className="leftPanel">
+                      <div className="textDoc" onMouseUp={this.onSelectText}>
+                          <Typography variant="body1" gutterBottom>
+                              {this.props.pdf.text}
+                          </Typography>
+                      </div>
+                  </Paper>
+
+                  <div className="rightPanel">
+                      <Sidebar selected_text={this.state.selected_text} pdf_document={this.state.pdf_document}
+                               id={this.props.pdf.id} resetSelection={this.resetSelection}/>
+                  </div>
+
+              </div>
+            );
+
+        }
+
+    }
 }
 
 
--- app/src/containers/Sidebar/index.js
+++ app/src/containers/Sidebar/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import SaveIcon from '@material-ui/icons/Save';
 import FormControl from "@material-ui/core/FormControl/FormControl";
 import InputLabel from "@material-ui/core/InputLabel/InputLabel";
@@ -6,11 +6,11 @@ import Select from "@material-ui/core/Select/Select";
 import MenuItem from "@material-ui/core/MenuItem/MenuItem";
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import { postAnnotations } from "../../store/actions/annotateActions";
+import {postAnnotations} from "../../store/actions/annotateActions";
 import Paper from '@material-ui/core/Paper';
-import { withStyles } from '@material-ui/core/styles';
-import { connect } from 'react-redux';
-import { fetchTagsAndDocRefs } from "../../store/actions/tagsActions"
+import {withStyles} from '@material-ui/core/styles';
+import {connect} from 'react-redux';
+import {fetchTagsAndDocRefs} from "../../store/actions/tagsActions"
 import "./index.css"
 
 
@@ -43,65 +43,71 @@ class Sidebar extends Component {
 
     dropdownHandleChange = (event) => {
         let tag_id = this.props.tags.filter(tag => tag.name === event.target.value)[0].id
-        this.setState({ document_tags: tag_id });
+        this.setState({document_tags: tag_id});
     };
 
     saveHandler = () => {
-        this.setState({
-            pdf_document: this.props.pdf
-        });
-
         this.props.dispatch(postAnnotations({
             selected_text: this.props.selected_text,
             document_tags: this.state.document_tags,
             pdf_documents: this.props.id,
         }))
+
+
+        this.setState({
+            pdf_document: this.props.pdf
+        });
+
+        this.props.resetSelection()
     };
+
     allDocumentHandler = () => {
         //  console.log(document.getElementById("text").focus())
     };
 
 
-    render() {
-        const { classes } = this.props;
+    render () {
+        const {classes} = this.props;
         return (
-            <div>
-                <Paper>
-                    <Button variant="outlined" color="secondary" className={classes.button} onClick={this.allDocumentHandler}>
-                        Select all text of the Document
-          </Button>
-                    <Typography variant="display1" gutterBottom>
-                        or
-          </Typography>
-                    <Typography variant="h3" gutterBottom>
-                        Selected Text:
-          </Typography>
-                    <div className="highlightedText" onChange={this.textHandler}>
-                        <Typography variant="body1" gutterBottom>{this.props.selected_text}</Typography>
-                    </div>
-                    <form className={classes.root} autoComplete="off">
-                        <FormControl className={classes.formControl}>
-                            <InputLabel htmlFor="tag_dropdown">Tags</InputLabel>
-                            <Select
-                                value={this.props.tags.filter(tag => tag.id === this.state.document_tags).length === 0 ? 'Select' : this.props.tags.filter(tag => tag.id === this.state.document_tags)[0].name}
-                                onChange={this.dropdownHandleChange}
-                                inputProps={{ name: 'tag' }}>
-                                {
-                                    this.props.tags.map((tag, index) => {
-                                        return <MenuItem key={index} value={tag.name}>{tag.name}</MenuItem>
-                                    })
-                                }
-                            </Select>
-                        </FormControl>
-                    </form>
-                    <Button variant="contained" size="small" className={classes.button} onClick={this.saveHandler}>
-                        <SaveIcon />
-                        Save
-          </Button>
-                </Paper>
-            </div>
+          <div>
+              <Paper>
+                  <Button variant="outlined" color="secondary" className={classes.button}
+                          onClick={this.allDocumentHandler}>
+                      Select all text of the Document
+                  </Button>
+                  <Typography variant="display1" gutterBottom>
+                      or
+                  </Typography>
+                  <Typography variant="h3" gutterBottom>
+                      Selected Text:
+                  </Typography>
+                  <div className="highlightedText" onChange={this.textHandler}>
+                      <Typography variant="body1" gutterBottom>{this.props.selected_text}</Typography>
+                  </div>
+                  <form className={classes.root} autoComplete="off">
+                      <FormControl className={classes.formControl}>
+                          <InputLabel htmlFor="tag_dropdown">Tags</InputLabel>
+                          <Select
+                            value={this.props.tags.filter(tag => tag.id === this.state.document_tags).length === 0 ? 'Select' : this.props.tags.filter(tag => tag.id === this.state.document_tags)[0].name}
+                            onChange={this.dropdownHandleChange}
+                            inputProps={{name: 'tag'}}>
+                              {
+                                  this.props.tags.map((tag, index) => {
+                                      return <MenuItem key={index} value={tag.name}>{tag.name}</MenuItem>
+                                  })
+                              }
+                          </Select>
+                      </FormControl>
+                  </form>
+                  <Button variant="contained" size="small" className={classes.button} onClick={this.saveHandler}>
+                      <SaveIcon/>
+                      Save
+                  </Button>
+              </Paper>
+          </div>
         );
     }
+
     componentDidMount = () => {
         this.props.dispatch(fetchTagsAndDocRefs())
     }
