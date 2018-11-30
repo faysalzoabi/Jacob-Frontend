5d13e7da48f5395afcf7a4b23e8a43d41f4b688e
Ian Stump <ianstump@gmail.com>
2018-11-30 17:01:08 +0100

    cleaned

--- app/src/index.js
+++ app/src/index.js
@@ -4,7 +4,6 @@ import App from './containers/App';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { MuiThemeProvider } from '@material-ui/core/styles';
-import Popup from 'react-popup';
 
 import './index.css';
 
